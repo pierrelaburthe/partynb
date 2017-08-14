ea5bf504bf66dad79e3c332b8cef352b2c4979d0
Pierre Donati <pierre.donati_arranz@edu.escpeurope.eu>
2017-08-14 17:20:01 +0200

    routes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,12 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :party
+
+  validates :user, presence: true
+  validates :user, uniqueness: true
+
+  validates :party, presence: true
+  validates :party, uniqueness: true
+
+  validates :participant_count, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :parties do
+    resources :bookings
+  end
+
+  resources :users, except: [:delete] do
+    resources :bookings, except: [:delete]
+  end
 end
