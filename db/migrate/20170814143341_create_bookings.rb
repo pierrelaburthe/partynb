f482efcd6f897760062d31da168f81c3a3bfb93c
Pierre Donati <pierre.donati_arranz@edu.escpeurope.eu>
2017-08-14 16:53:00 +0200

    Booking model created

---
+++ db/migrate/20170814143341_create_bookings.rb
@@ -0,0 +1,12 @@
+class CreateBookings < ActiveRecord::Migration[5.0]
+  def change
+    create_table :bookings do |t|
+      t.references :user, foreign_key: true
+      t.references :party, foreign_key: true
+      t.integer :participant_count
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
