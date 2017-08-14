a2304cfc18254091826708b1a431be231724dd42
Pierre Donati <pierre.donati_arranz@edu.escpeurope.eu>
2017-08-14 19:25:43 +0200

    show user not finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-has_many :parties
+has_many :parties, through: :bookings
+has_many :bookings
+
 validates :name, presence: true
 validates :mail, presence: true
 validates :password, presence: true
