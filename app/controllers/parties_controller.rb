01adcf1ff02682b6d889f81b373497cfff363338
Elliot Seror <elliot.seror@edhec.com>
2017-08-14 17:14:27 +0200

    parties controller completed. waiting for routes to continue

---
+++ app/controllers/parties_controller.rb
@@ -0,0 +1,48 @@
+class PartiesController < ApplicationController
+
+before_action :set_party, only: [:show, :edit, :destroy]
+
+  def index
+    @parties = Party.all
+  end
+
+  def show
+    @party = Party.new
+  end
+
+  def new
+    @party= Party.new
+  end
+
+  def create
+    @party= Party.new(party_params)
+    if @party.save
+     redirect_to parties_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @party= party.update(party_params)
+    redirect_to party_path(@party)
+  end
+
+  def destroy
+    @party.destroy
+     redirect_to parties_path
+  end
+
+  private
+  def set_party
+    @party = party.find(params[:id])
+  end
+
+  def party_params
+    params.require(:party).permit(:title, :capacity, :music_type, :price, :occurs_at, :user_id)
+  end
+
+end
--- app/models/party.rb
+++ app/models/party.rb
@@ -1,5 +1,5 @@
 class Party < ApplicationRecord
   belongs_to :user
   has_many :users
-
+  validates :title, presence: true , allow_blank: false
 end
