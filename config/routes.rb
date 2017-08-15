144f9c17cac73c8570e73e9621eeb487654cea7f
Pierre Laburthe <laburthe35@gmail.com>
2017-08-15 11:46:00 +0200

    facebook login works git add . git add .

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
 
   resources :parties do
@@ -9,4 +10,5 @@ Rails.application.routes.draw do
   resources :users, except: [:delete]
 
 end
+
 
