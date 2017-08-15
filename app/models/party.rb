b4bad7ae31c3b141ab703708698abd158fc080fb
Pierre Donati <pierre.donati_arranz@edu.escpeurope.eu>
2017-08-15 10:32:05 +0200

    authentication beginning

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,5 +1,4 @@
 class Party < ApplicationRecord
   belongs_to :user
-  has_many :users
   validates :title, presence: true , allow_blank: false
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
-has_many :parties, through: :bookings
-has_many :bookings
-
-validates :name, presence: true
-validates :mail, presence: true
-validates :password, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
