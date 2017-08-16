72e2bf0642b4c3a09331208fcd25cb848b8578d8
Pierre Laburthe <laburthe35@gmail.com>
2017-08-16 16:02:50 +0200

    google map setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,4 @@
 //= require_tree .
 //= require underscore
 //= require gmaps/google
+<%= javascript_include_tag "https://maps.google.com/maps/api/js?libraries=places&key=#{ENV['GOOGLE_API_BROWSER_KEY']}" %>
