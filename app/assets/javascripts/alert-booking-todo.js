dd7cb3949ed45ff316b805e03c64faa662251506
Pierre Laburthe <laburthe35@gmail.com>
2017-08-17 11:19:25 +0200

    map fixed

---
+++ app/assets/javascripts/alert-booking-todo.js
@@ -0,0 +1,9 @@
+
+const button = document.getElementById("bookbutton");
+const field  = document.getElementById("alertmsg");
+
+// button.addEventListener(
+//   "click",
+//   (event) => {  field.addToclass{just-booked}  };
+// alertmsg.innerHTML = "Your booking has been made!"
+// });
