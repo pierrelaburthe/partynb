1778897a849ff63d924521a93225485717e5b844
Pierre Donati <pierre.donati_arranz@edu.escpeurope.eu>
2017-08-18 13:45:48 +0200

    some back

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+   def photo_url(object, options = {}, missing_url="https://static.pexels.com/photos/7283/garden.jpg" )
+    if object.photo_url
+      return cl_image_path(object.photo, options)
+    else
+      photo_url = image_path(missing_url)
+    end
+  end
+
 end
--- app/models/party.rb
+++ app/models/party.rb
@@ -8,4 +8,5 @@ mount_uploader :photo, PhotoUploader
  has_many :bookings, dependent: :destroy
  has_many :users, through: :bookings
  validates :title, presence: true , allow_blank: false
+
 end
